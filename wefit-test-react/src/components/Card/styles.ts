93eb3d6297aee1c66770df62e43b27aee6ca57c3
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2024-03-21 03:09:07 -0300

    ref: create card component

---
+++ wefit-test-react/src/components/Card/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.375rem;
+  background-color: #ffffff;
+  color: #2d3748;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1);
+  padding: 16px;  
+  width: auto;
+`;
+
+export {
+  Card
+}
